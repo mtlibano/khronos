fd57dc696f77d38a63178be6d95f2ebe065733f9
Max Tiago Libano Alves Cordeiro <maxtlac@gmail.com>
2023-08-17 09:16:52 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,80 @@
+const minutesEl = document.querySelector("#minutes")
+const secondsEl = document.querySelector("#seconds")
+const millisecondsEl = document.querySelector("#milliseconds")
+const startBtn = document.querySelector("#startBtn")
+const resumeBtn = document.querySelector("#resumeBtn")
+const pauseBtn = document.querySelector("#pauseBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+resumeBtn.addEventListener("click", resumeTimer);
+resetBtn.addEventListener("click", resetTimer);
+
+function startTimer() {
+
+  isPaused = false;
+
+  interval = setInterval(() => {
+
+    if(!isPaused) {
+      milliseconds += 10
+      if(milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
+      }
+      if(seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+
+      minutesEl.textContent = formatTimer(minutes);
+      secondsEl.textContent = formatTimer(seconds);
+      millisecondsEl.textContent = formatMilliseconds(milliseconds);
+    }
+  }, 10)
+
+  startBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+}
+
+function pauseTimer() {
+  isPaused = true;
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "block";
+}
+
+function resumeTimer() {
+  isPaused = false;
+  resumeBtn.style.display = "none";
+  pauseBtn.style.display = "block";
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+
+  minutesEl.textContent = "00";
+  secondsEl.textContent = "00";
+  millisecondsEl.textContent = "000";
+
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "none";
+  startBtn.style.display = "block";
+}
+
+function formatTimer(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+function formatMilliseconds(time) {
+  return time < 100 ? `${time}`.padStart(3, "0") : time;
+}
